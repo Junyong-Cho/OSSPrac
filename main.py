0d02d9d16471bda2dbb634305fcc3bbb4f58dd99
ChoJunyong <junyong4510@gmail.com>
2025-03-31 17:39:01 +0900

    2025-03-31 개인과제5 5번

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 name = input('이름을 입력하세요: ')
-no = int(input('학번을 입력하세요: '))
+no = input('학번을 입력하세요: ')
+major = input('학과를 입력하세요: ')
 
 print('<출력>')
-print('이름: {}\n학번: {}'.format(name,no))
+print('이름: {}\n학번: {}\n학과: {}'.format(name,no,major))
